2935146f9216f25ccb990e0b383d787ad23d806c
keyboardsage <keyboardsage@users.noreply.github.com>
2024-02-20 18:00:26 -0500

    Moved main.cpp to its own test directory

---
+++ tests/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "ogg/ogg.h"
+
+int main() {
+    //const char* oggVersion = ogg_version_string();
+    std::cout << "Linked libogg version: " << std::endl;
+
+    return EXIT_SUCCESS;
+}
